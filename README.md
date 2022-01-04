f44e3fd166141980ca112824ce81dc7823b8c3bf
mostafa elbannan <mostafaelbannan1@gmail.com>
2022-01-03 18:56:41 -0800

    got basic block moving

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # portfolio
 A portfolio website in the form of a game
+
+# npm Commands
+
+To install dependencies
+```
+npm install
+```
--- src/index.js
+++ src/index.js
@@ -1,24 +1,60 @@
-import "./x.js"
-import * as three from "https://cdn.skypack.dev/three@0.136.0"
+import * as three from "../node_modules/three/build/three.module.js"
 
-const renderer = new three.WebGLRenderer()
+const canvas = document.getElementById("app")
+const renderer = new three.WebGLRenderer({ canvas })
 renderer.setSize(window.innerWidth, window.innerHeight)
-window.document.body.appendChild(renderer.domElement)
+renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
 
 const camera = new three.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000)
-camera.position.z = 5
+camera.position.set(1, 2.6, -3)
+camera.lookAt(0, 0, 0)
 
 const scene = new three.Scene()
 
-const geometry = new three.BoxGeometry()
-const material = new three.MeshBasicMaterial({ color: 0x00ff00 })
-const cube = new three.Mesh(geometry, material)
-scene.add(cube)
+const gamePlaneGeometry = new three.PlaneBufferGeometry(3, 3, 64, 64)
+const gamePlaneMaterial = new three.MeshBasicMaterial({ color: "gray" })
+const gamePlane = new three.Mesh(gamePlaneGeometry, gamePlaneMaterial)
+gamePlane.material.side = three.DoubleSide
+gamePlane.rotation.x = 55
+scene.add(gamePlane)
+
+const playerGeometry = new three.BoxBufferGeometry(0.5, 0.5, 0.5)
+const playerMaterial = new three.MeshBasicMaterial({ color: "red" })
+const player = new three.Mesh(playerGeometry, playerMaterial)
+player.position.set(0, 0.3, 0)
+scene.add(player)
+
+const upKey = 38
+const downKey = 40
+const leftKey = 37
+const rightKey = 39
+const spaceKey = 32
+
+function move(event) {
+    const code = event.keyCode
+    if (code === upKey) {
+        player.position.z += 0.1
+    } else if (code === downKey) {
+        player.position.z -= 0.1
+    } else if (code === leftKey) {
+        player.position.x += 0.1
+    } else if (code === rightKey) {
+        player.position.x -= 0.1
+    } else if (code === spaceKey) {
+        player.position.y += 0.2
+    }
+}
+
+window.addEventListener("keydown", move)
+window.addEventListener("resize", () => {
+    camera.aspect = window.innerWidth / window.innerHeight
+    camera.updateProjectionMatrix()
+    renderer.setSize(window.innerWidth, window.innerHeight)
+    renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
+})
 
 function animate() {
     requestAnimationFrame(animate)
-    cube.rotation.x += 0.01
-    cube.rotation.y += 0.01
     renderer.render(scene, camera)
 }
 
